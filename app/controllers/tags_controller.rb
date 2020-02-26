32aa03c56f31f5ba6a1fd0e3db66c0f22763c4ef
tjustino <thomas.justino@gmail.com>
2020-02-26 23:14:22 +0100

    manage tags has is should be

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,15 +1,12 @@
+# frozen_string_literal: true
+
+# Tags Controller
 class TagsController < ApplicationController
-  before_action :set_tag, only: [:edit, :update, :destroy]
+  before_action :set_tag, only: %i[edit update destroy]
+  before_action :set_meta_tags, only: %i[index new edit create update]
 
   # GET /tags
-  def index
-    @meta_tags = MetaTag.all
-    # @tags = Tag.all
-  end
-
-  # GET /tags/1
-  def show
-  end
+  def index; end
 
   # GET /tags/new
   def new
@@ -17,15 +14,14 @@ class TagsController < ApplicationController
   end
 
   # GET /tags/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /tags
   def create
     @tag = Tag.new(tag_params)
 
     if @tag.save
-      redirect_to @tag, notice: 'Tag was successfully created.'
+      redirect_to tags_url, notice: notice_message("créée")
     else
       render :new
     end
@@ -34,7 +30,7 @@ class TagsController < ApplicationController
   # PATCH/PUT /tags/1
   def update
     if @tag.update(tag_params)
-      redirect_to @tag, notice: 'Tag was successfully updated.'
+      redirect_to tags_url, notice: notice_message("mise à jour")
     else
       render :edit
     end
@@ -42,11 +38,16 @@ class TagsController < ApplicationController
 
   # DELETE /tags/1
   def destroy
-    @tag.destroy
-    redirect_to tags_url, notice: 'Tag was successfully destroyed.'
+    if @tag.destroy
+      redirect_to tags_url, notice: notice_message("supprimée")
+    else
+      redirect_to tags_url,
+                  warning: "Impossible de surpprimer « #{@tag.name} »."
+    end
   end
 
-  private
+  private ######################################################################
+
     # Use callbacks to share common setup or constraints between actions.
     def set_tag
       @tag = Tag.find(params[:id])
@@ -55,5 +56,13 @@ class TagsController < ApplicationController
     # Only allow a trusted parameter "white list" through.
     def tag_params
       params.require(:tag).permit(:name, :meta_tag_id)
+    end
+
+    def set_meta_tags
+      @meta_tags = MetaTag.all.order(:name)
+    end
+
+    def notice_message(action)
+      "L'étiquette « #{@tag.name} » a été #{action}."
     end
 end
