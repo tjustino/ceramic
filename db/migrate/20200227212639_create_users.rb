a4fc2264c9086ba87bb171f489350868c8ae863a
tjustino <thomas.justino@gmail.com>
2020-03-09 22:12:27 +0100

    style

--- db/migrate/20200227212639_create_users.rb
+++ db/migrate/20200227212639_create_users.rb
@@ -4,13 +4,13 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
-      t.string :email, null: false#, index: { unique: true }
+      t.string :email, null: false
       t.string :password_digest, null: false
       t.boolean :is_admin, default: false
 
       t.timestamps
-    end
 
-    add_index :users, :email, unique: true
+      t.index [:email], unique: true
+    end
   end
 end
