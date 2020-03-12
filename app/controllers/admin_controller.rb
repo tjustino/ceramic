c2c56a16bee7b17464a881c2fd54bdc44acc9b66
tjustino <thomas.justino@gmail.com>
2020-03-12 14:53:16 +0100

    restrict access for admin views

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -3,6 +3,7 @@
 # Admin Controller
 class AdminController < ApplicationController
   include CurrentCart
+  before_action :restrict_access
   before_action :set_cart, only: [:index]
 
   def index; end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   add_flash_types :warning
+
+  def restrict_access
+    unless User.find(session[:user_id]).is_admin?
+      redirect_to root_url
+    end
+  end
 end
