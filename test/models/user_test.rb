14534cb4ac9461fe9c50847fb3112d3abdfad5a8
tjustino <thomas.justino@gmail.com>
2020-03-16 21:59:14 +0100

    add Schema Information

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  email           :string           not null
+#  password_digest :string           not null
+#  is_admin        :boolean          default("0")
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
