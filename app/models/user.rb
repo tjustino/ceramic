40806f8d1595b39e1f9e80b8cf48795a5b6f6e26
tjustino <thomas.justino@gmail.com>
2020-03-16 22:09:52 +0100

    add Schema Information

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  email           :string           not null
+#  password_digest :string           not null
+#  is_admin        :boolean          default("0")
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
 class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true, format: {
     with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
