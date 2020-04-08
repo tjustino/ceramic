880aa1868c062a53cf655f7938ad3b3b5e174ad6
tjustino <thomas.justino@gmail.com>
2020-04-08 22:58:01 +0200

    fix when to show banners

--- app/controllers/concerns/banners.rb
+++ app/controllers/concerns/banners.rb
@@ -5,7 +5,7 @@ module Banners
   private
 
     def load_banners
-      now = DateTime.now
-      @banners = Banner.where(["start < :now and end > :now", { now: now }])
+      now = Date.current
+      @banners = Banner.where(["start <= :now and end >= :now", { now: now }])
     end
 end
