711904a188c6b07d85e5c48d07b7b0941a4d6e16
tjustino <thomas.justino@gmail.com>
2020-04-13 23:04:28 +0200

    complete admin controller test

--- test/controllers/admin_controller_test.rb
+++ test/controllers/admin_controller_test.rb
@@ -4,8 +4,20 @@ require "test_helper"
 
 # Admin Controller Test
 class AdminControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get admin_index_url
+  test "should redirect index as regular" do
+    get admin_url
+    assert_redirected_to login_url
+  end
+
+  test "should redirect index as customer" do
+    login_as(:customer)
+    get admin_url
+    assert_redirected_to login_url
+  end
+
+  test "should get index as admin" do
+    login_as(:admin)
+    get admin_url
     assert_response :success
   end
 end
