cf85a2bbe1ae9928ff7bd5edfd158dc45dc6e21c
tjustino <thomas.justino@gmail.com>
2020-04-13 23:03:52 +0200

    ActionCable is not active for the moment

--- test/channels/application_cable/connection_test.rb
+++ test/channels/application_cable/connection_test.rb
@@ -2,14 +2,14 @@
 
 require "test_helper"
 
-module ApplicationCable
-  # class ConnectionTest < ActionCable::Connection::TestCase
-  #   test "connects with cookies" do
-  #     cookies.signed[:user_id] = 42
-  #
-  #     connect
-  #
-  #     assert_equal connection.user_id, "42"
-  #   end
-  # end
-end
+# module ApplicationCable
+#   class ConnectionTest < ActionCable::Connection::TestCase
+#     test "connects with cookies" do
+#       cookies.signed[:user_id] = 42
+#
+#       connect
+#
+#       assert_equal connection.user_id, "42"
+#     end
+#   end
+# end
