249d088406442414d6c038273da6723014e909fa
tjustino <thomas.justino@gmail.com>
2020-04-14 21:46:48 +0200

    Meta Tags tests

--- test/controllers/meta_tags_controller_test.rb
+++ test/controllers/meta_tags_controller_test.rb
@@ -5,49 +5,112 @@ require "test_helper"
 # Meta Tags Controller Test
 class MetaTagsControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @meta_tag = meta_tags(:one)
+    @meta_tag = meta_tags(:bijoux)
   end
 
-  test "should get index" do
+  ############################################################### GET /meta_tags
+  test "should redirect index as visitor and customer" do
+    %i[visitor customer].each do |not_admin_user|
+      login_as(not_admin_user)
+      get meta_tags_url
+      assert_redirected_to login_url
+    end
+  end
+
+  test "should get index as admin" do
+    login_as(:admin)
     get meta_tags_url
     assert_response :success
   end
 
+  ########################################################### GET /meta_tags/new
+  test "should redirect new as visitor and customer" do
+    %i[visitor customer].each do |not_admin_user|
+      login_as(not_admin_user)
+      get new_meta_tag_url
+      assert_redirected_to login_url
+    end
+  end
+
   test "should get new" do
+    login_as(:admin)
     get new_meta_tag_url
     assert_response :success
   end
 
-  test "should create meta_tag" do
-    assert_difference("MetaTag.count") do
-      post meta_tags_url, params: { meta_tag: { name: @meta_tag.name } }
+  ######################################################## GET /meta_tags/1/edit
+  test "should redirect edit as visitor and customer" do
+    %i[visitor customer].each do |not_admin_user|
+      login_as(not_admin_user)
+      get edit_meta_tag_url(@meta_tag)
+      assert_redirected_to login_url
     end
-
-    assert_redirected_to meta_tag_url(MetaTag.last)
   end
 
-  test "should show meta_tag" do
-    get meta_tag_url(@meta_tag)
-    assert_response :success
-  end
-
-  test "should get edit" do
+  test "should get edit as admin" do
+    login_as(:admin)
     get edit_meta_tag_url(@meta_tag)
     assert_response :success
   end
 
-  test "should update meta_tag" do
-    patch meta_tag_url(@meta_tag), params: {
-      meta_tag: { name: @meta_tag.name }
-    }
-    assert_redirected_to meta_tag_url(@meta_tag)
+  ############################################################## POST /meta_tags
+  test "should redirect create as visitor and customer" do
+    %i[visitor customer].each do |not_admin_user|
+      login_as(not_admin_user)
+      assert_no_difference("MetaTag.count") { post_create }
+      assert_redirected_to login_url
+    end
   end
 
-  test "should destroy meta_tag" do
-    assert_difference("MetaTag.count", -1) do
-      delete meta_tag_url(@meta_tag)
-    end
-
+  test "should create meta_tag as admin" do
+    login_as(:admin)
+    assert_difference("MetaTag.count", +1) { post_create }
     assert_redirected_to meta_tags_url
   end
+
+  ####################################################### PATCH/PUT /meta_tags/1
+  test "should redirect update as visitor and customer" do
+    %i[visitor customer].each do |not_admin_user|
+      login_as(not_admin_user)
+      patch_update(@meta_tag)
+      assert_redirected_to login_url
+      assert_equal @meta_tag.name, @meta_tag.reload.name
+    end
+  end
+
+  test "should update meta_tag as admin" do
+    login_as(:admin)
+    patch_update(@meta_tag)
+    assert_redirected_to meta_tags_url
+    assert_not_equal @meta_tag.name, @meta_tag.reload.name
+  end
+
+  ########################################################## DELETE /meta_tags/1
+  test "should redirect destroy as visitor and customer" do
+    %i[visitor customer].each do |not_admin_user|
+      login_as(not_admin_user)
+      assert_no_difference("MetaTag.count") { delete meta_tag_url(@meta_tag) }
+      assert_redirected_to login_url
+    end
+  end
+
+  test "should destroy meta_tag as admin" do
+    login_as(:admin)
+    ProductTagRelation.destroy_all
+    @meta_tag.tags.destroy_all
+    assert_difference("MetaTag.count", -1) { delete meta_tag_url(@meta_tag) }
+    assert_redirected_to meta_tags_url
+  end
+
+  private ######################################################################
+
+    def post_create
+      post meta_tags_url, params: { meta_tag: { name: SecureRandom.hex } }
+    end
+
+    def patch_update(meta_tag)
+      patch meta_tag_url(meta_tag), params: {
+        meta_tag: { name: SecureRandom.hex }
+      }
+    end
 end
