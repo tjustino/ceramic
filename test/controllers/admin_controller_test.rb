a82d901db0bd300a6cecd6e8a3ef156dd9a5d7bb
tjustino <thomas.justino@gmail.com>
2020-04-14 17:10:16 +0200

    refactoring redirection's tests

--- test/controllers/admin_controller_test.rb
+++ test/controllers/admin_controller_test.rb
@@ -5,15 +5,12 @@ require "test_helper"
 # Admin Controller Test
 class AdminControllerTest < ActionDispatch::IntegrationTest
   ################################################################### GET /admin
-  test "should redirect index as regular" do
-    get admin_url
-    assert_redirected_to login_url
-  end
-
-  test "should redirect index as customer" do
-    login_as(:customer)
-    get admin_url
-    assert_redirected_to login_url
+  test "should redirect index as visitor and customer" do
+    %i[visitor customer].each do |not_admin_user|
+      login_as(not_admin_user)
+      get admin_url
+      assert_redirected_to login_url
+    end
   end
 
   test "should get index as admin" do
