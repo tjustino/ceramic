ce5b77bb9093b60c71c323b0a291d347a497f262
tjustino <thomas.justino@gmail.com>
2020-04-14 17:15:35 +0200

    home tests

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -4,8 +4,12 @@ require "test_helper"
 
 # Home Controller Test
 class HomeControllerTest < ActionDispatch::IntegrationTest
+  ######################################################################## GET /
   test "should get index" do
-    get home_index_url
-    assert_response :success
+    %i[visitor customer admin].each do |any_user|
+      login_as(any_user)
+      get root_url
+      assert_response :success
+    end
   end
 end
