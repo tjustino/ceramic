e39d0b1b0b3d29250a07c5043a47f63bf9c605e3
tjustino <thomas.justino@gmail.com>
2020-04-16 14:54:23 +0200

    when "resources :statics, only: %i[show]" come after scoping "resources :statics", static_path generate /admin/statics/:id

--- config/routes.rb
+++ config/routes.rb
@@ -3,19 +3,6 @@
 Rails.application.routes.draw do
   # For details on the DSL, see https://guides.rubyonrails.org/routing.html
 
-  ######################################################################## admin
-  get "admin", to: "admin#index"
-
-  scope :admin do
-    resources :banners
-    resources :carts,     only: %i[index new create edit update destroy]
-    resources :meta_tags, except: [:show]
-    resources :products,  only: %i[index new create edit update destroy]
-    resources :statics,   only: %i[index edit update]
-    resources :tags,      except: [:show]
-    resources :users,     only:   [:index]
-  end
-
   ####################################################################### public
   resources :carts, only: [:show]
   root "home#index"
@@ -31,4 +18,17 @@ Rails.application.routes.draw do
 
   resources :statics, only: %i[show]
   resources :users,   only: %i[new create edit update destroy]
+
+  ######################################################################## admin
+  get "admin", to: "admin#index"
+
+  scope :admin do
+    resources :banners
+    resources :carts,     only: %i[index new create edit update destroy]
+    resources :meta_tags, except: [:show]
+    resources :products,  only: %i[index new create edit update destroy]
+    resources :statics,   only: %i[index edit update]
+    resources :tags,      except: [:show]
+    resources :users,     only:   [:index]
+  end
 end
