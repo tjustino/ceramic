5cead24d0db934bf9d4199766c15104c04c08b33
tjustino <thomas.justino@gmail.com>
2020-04-17 12:03:23 +0200

    Statics tests

--- test/controllers/statics_controller_test.rb
+++ test/controllers/statics_controller_test.rb
@@ -5,47 +5,71 @@ require "test_helper"
 # Statics Controller Test
 class StaticsControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @static = statics(:one)
+    @static = statics(:terms)
   end
 
-  test "should get index" do
+  ################################################################# GET /statics
+  test "should redirect index as visitor and customer" do
+    %i[visitor customer].each do |not_admin_user|
+      login_as(not_admin_user)
+      get statics_url
+      assert_redirected_to login_url
+    end
+  end
+
+  test "should get index as admin" do
+    login_as(:admin)
     get statics_url
     assert_response :success
   end
 
-  test "should get new" do
-    get new_static_url
-    assert_response :success
-  end
-
-  test "should create static" do
-    assert_difference("Static.count") do
-      post statics_url, params: { static: { page: @static.page } }
-    end
-
-    assert_redirected_to static_url(Static.last)
-  end
-
+  ############################################################### GET /statics/1
+  # https://github.com/rails/rails/issues/38973
   test "should show static" do
-    get static_url(@static)
-    assert_response :success
+    %i[visitor customer admin].each do |any_user|
+      login_as(any_user)
+      get static_url(@static)
+      assert_response :success
+    end
   end
 
-  test "should get edit" do
+  ########################################################## GET /statics/1/edit
+  test "should redirect edit as visitor and customer" do
+    %i[visitor customer].each do |not_admin_user|
+      login_as(not_admin_user)
+      get edit_static_url(@static)
+      assert_redirected_to login_url
+    end
+  end
+
+  test "should get edit as admin" do
+    login_as(:admin)
     get edit_static_url(@static)
     assert_response :success
   end
 
-  test "should update static" do
-    patch static_url(@static), params: { static: { page: @static.page } }
-    assert_redirected_to static_url(@static)
-  end
-
-  test "should destroy static" do
-    assert_difference("Static.count", -1) do
-      delete static_url(@static)
+  ######################################################### PATCH/PUT /statics/1
+  test "should redirect update as visitor and customer" do
+    %i[visitor customer].each do |not_admin_user|
+      original_content = @static.content.to_s
+      login_as(not_admin_user)
+      patch static_url(@static), params: static_params
+      assert_redirected_to login_url
+      assert_equal original_content, @static.reload.content.to_s
     end
-
-    assert_redirected_to statics_url
   end
+
+  test "should update static as admin" do
+    original_content = @static.content.to_s
+    login_as(:admin)
+    patch static_url(@static), params: static_params
+    assert_redirected_to statics_url
+    assert_not_equal original_content, @static.reload.content.to_s
+  end
+
+  private ####################################################################
+
+    def static_params
+      { static: { page: @static.page, content: SecureRandom.hex } }
+    end
 end
