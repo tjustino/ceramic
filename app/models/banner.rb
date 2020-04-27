82deee14ac4d881108ee6196dd10056193cf5d72
tjustino <thomas.justino@gmail.com>
2020-04-27 14:31:57 +0200

    add a date consistency validation method

--- app/models/banner.rb
+++ app/models/banner.rb
@@ -6,13 +6,24 @@
 #
 #  id         :integer          not null, primary key
 #  message    :text             not null
-#  start      :date             not null
-#  end        :date             not null
+#  start_date :date             not null
+#  end_date   :date             not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
 class Banner < ApplicationRecord
-  validates :message, presence: true
-  validates :start,   presence: true
-  validates :end,     presence: true
+  validates :message,    presence: true
+  validates :start_date, presence: true
+  validates :end_date,   presence: true
+  validate  :date_consistency
+
+  private ######################################################################
+
+    def date_consistency
+      return if start_date.blank? || end_date.blank?
+
+      if start_date > end_date
+        errors.add(:start_date, "incohérence dans les dates")
+      end
+    end
 end
