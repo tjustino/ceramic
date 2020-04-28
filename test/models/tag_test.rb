1a080ecca3f4d2ee72a895986af702b9e0783e0c
tjustino <thomas.justino@gmail.com>
2020-04-28 10:34:38 +0200

    Tag model tests

--- test/models/tag_test.rb
+++ test/models/tag_test.rb
@@ -12,8 +12,33 @@
 #
 require "test_helper"
 
+# Tag Tests
 class TagTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should create tag" do
+    tag = Tag.new(name: rand_text, meta_tag_id: rand_meta_tag)
+    assert tag.valid?
+  end
+
+  test "should not create tag with not null data" do
+    missing_name = Tag.new(name: nil, meta_tag_id: rand_meta_tag)
+    assert missing_name.invalid?
+    assert_equal ["ne peut pas être vide"], missing_name.errors[:name]
+
+    missing_mt_id = Tag.new(name: rand_text, meta_tag_id: nil)
+    assert missing_mt_id.invalid?
+    assert_equal ["ne peut pas être vide"], missing_mt_id.errors[:meta_tag_id]
+  end
+
+  # TODO: test product_tag_relations, dependent: :restrict_with_error
+
+  private ######################################################################
+
+    def rand_text
+      SecureRandom.hex
+    end
+
+    def rand_meta_tag
+      offset = rand(MetaTag.count)
+      MetaTag.offset(offset).first.id
+    end
 end
